922339a828cdcd2bf4e5e2bf89705e93c16354f5
Ilnur Fattakhov <nurich1995@mail.ru>
2023-11-17 18:35:55 +0300

    Add new application MyPlaces

---
+++ MyPlaces/ViewControllers/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Ilnur on 17.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        cell?.textLabel?.text = "Cell"
+        return cell!
+    }
+
+}
+
